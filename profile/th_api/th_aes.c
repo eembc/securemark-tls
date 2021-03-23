11cfa098afa1146f174f230bd51c461e8c35887b
petertorelli <peter.j.torelli@gmail.com>
2021-03-22 17:11:58 -0700

    Added AES/GCM.

--- profile/th_api/th_aes.c
+++ profile/th_api/th_aes.c
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) EEMBC(R). All Rights Reserved
- * 
+ *
  * All EEMBC Benchmark Software are products of EEMBC and are provided under the
  * terms of the EEMBC Benchmark License Agreements. The EEMBC Benchmark Software
  * are proprietary intellectual properties of EEMBC and its Members and is
- * protected under all applicable laws, including all applicable copyright laws.  
- * 
+ * protected under all applicable laws, including all applicable copyright laws.
+ *
  * If you received this EEMBC Benchmark Software without having a currently
  * effective EEMBC Benchmark License Agreement, you must discontinue use.
  */
@@ -136,8 +136,50 @@ th_aes128_ccm_decrypt(
 }
 
 /**
+ * Perform an AES/GCM encrypt.
+ *
+ * Return EE_STATUS_OK or EE_STATUS_ERROR.
+ */
+ee_status_t
+th_aes128_gcm_encrypt(
+    void                *p_context, // input: portable context
+    const unsigned char *p_pt,      // input: plaintext
+    unsigned int         ptlen,     // input: length of plaintext in bytes
+    unsigned char       *p_ct,      // output: ciphertext
+    unsigned char       *p_tag,     // output: tag
+    unsigned int         taglen,    // input: tag length in bytes
+    unsigned char       *p_iv,      // input: initialization vector
+    unsigned int         ivlen      // input: IV length in bytes
+)
+{
+    #warning "th_aes128_gcm_encrypt not implemented"
+    return EE_STATUS_OK;
+}
+
+/**
+ * Perform an AES/GCM decrypt.
+ *
+ * Return EE_STATUS_OK or EE_STATUS_ERROR.
+ */
+ee_status_t
+th_aes128_gcm_decrypt(
+    void                *p_context, // input: portable context
+    const unsigned char *p_ct,      // input: ciphertext
+    unsigned int         ctlen,     // input: length of plaintext in bytes
+    unsigned char       *p_pt,      // output: plaintext
+    unsigned char       *p_tag,     // output: tag
+    unsigned int         taglen,    // input: tag length in bytes
+    unsigned char       *p_iv,      // input: initialization vector
+    unsigned int         ivlen      // input: IV length in bytes
+)
+{
+    #warning "th_aes128_gcm_decrypt not implemented"
+    return EE_STATUS_OK;
+}
+
+/**
  * Clean up the context created.
- * 
+ *
  * Indicate the mode that was used for _create()
  */
 void
